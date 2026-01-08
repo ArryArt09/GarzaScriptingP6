625742e2f7844a0bc4be234c9b872e8150aa4ad5
ArryArt09 <746785@student.dusd.net>
2026-01-08 13:49:47 -0800

    awoken
    learned how to awake stuff

---
+++ P6GarzaScriptingProject/Assets/Scripts/Awaking.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awaking : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Awakie is running: ");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start is running: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
